f82a07f222f196d200d3737d84bd05cc17df8ec8
robertinsmartinvestor-gif <robertin.smartinvestor@gmail.com>
2026-06-21 23:39:06 +0200

    Add files via upload

--- android/app/src/main/java/com/lepefylabs/sobre/MainActivity.java
+++ android/app/src/main/java/com/lepefylabs/sobre/MainActivity.java
@@ -1,5 +1,33 @@
 package com.lepefylabs.sobre;
 
+import android.content.Intent;
+import android.net.Uri;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(android.os.Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        handleDeepLink(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        handleDeepLink(intent);
+    }
+
+    private void handleDeepLink(Intent intent) {
+        if (intent == null) return;
+        Uri data = intent.getData();
+        if (data == null) return;
+        String host = data.getHost();
+        if (host == null) return;
+        if (!host.equals("www.sobrewellness.app") && !host.equals("sobrewellness.app")) return;
+
+        String url = data.toString();
+        getBridge().getWebView().post(() -> getBridge().getWebView().loadUrl(url));
+    }
+}
